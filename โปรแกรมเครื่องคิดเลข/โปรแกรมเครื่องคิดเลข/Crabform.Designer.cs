db49d3799f11afaf28a64bf858d62c4422b48f02
Kuro_kitten <115644649+Kuro-kittendotcom@users.noreply.github.com>
2023-03-21 22:57:42 +0700

    Add new VS project
    เพิ่มโปรเจ็กต์สำหรับโปรแกรมคิดเลข

---
+++ โปรแกรมเครื่องคิดเลข/โปรแกรมเครื่องคิดเลข/Crabform.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace โปรแกรมเครื่องคิดเงิน
+{
+    partial class Crabform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.subButton = new System.Windows.Forms.Button();
+            this.multiButton = new System.Windows.Forms.Button();
+            this.divButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 23);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 17);
+            this.label1.Size = new System.Drawing.Size(119, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "จำนวนที่ 1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 73);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(0, 0, 0, 25);
+            this.label2.Size = new System.Drawing.Size(119, 58);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "จำนวนที่ 2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 125);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(0, 0, 0, 8);
+            this.label3.Size = new System.Drawing.Size(90, 41);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ผลลัพธ์";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(164, 23);
+            this.textBox1.MaxLength = 20;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(315, 39);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Location = new System.Drawing.Point(164, 72);
+            this.textBox2.MaxLength = 20;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(315, 39);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyUp);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(164, 129);
+            this.textBox3.MaxLength = 20;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ShortcutsEnabled = false;
+            this.textBox3.Size = new System.Drawing.Size(315, 39);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TabStop = false;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // addButton
+            // 
+            this.addButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.addButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.addButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.addButton.ForeColor = System.Drawing.Color.Black;
+            this.addButton.Location = new System.Drawing.Point(510, 19);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(110, 45);
+            this.addButton.TabIndex = 6;
+            this.addButton.Text = "+";
+            this.addButton.UseVisualStyleBackColor = false;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            this.addButton.KeyUp += new System.Windows.Forms.KeyEventHandler(this.addButton_KeyUp);
+            // 
+            // subButton
+            // 
+            this.subButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.subButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.subButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.subButton.ForeColor = System.Drawing.Color.Black;
+            this.subButton.Location = new System.Drawing.Point(647, 19);
+            this.subButton.Name = "subButton";
+            this.subButton.Size = new System.Drawing.Size(110, 45);
+            this.subButton.TabIndex = 7;
+            this.subButton.Text = "-";
+            this.subButton.UseVisualStyleBackColor = false;
+            this.subButton.Click += new System.EventHandler(this.subButton_Click);
+            this.subButton.KeyUp += new System.Windows.Forms.KeyEventHandler(this.subButton_KeyUp);
+            // 
+            // multiButton
+            // 
+            this.multiButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.multiButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.multiButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.multiButton.ForeColor = System.Drawing.Color.Black;
+            this.multiButton.Location = new System.Drawing.Point(510, 72);
+            this.multiButton.Name = "multiButton";
+            this.multiButton.Size = new System.Drawing.Size(110, 45);
+            this.multiButton.TabIndex = 8;
+            this.multiButton.Text = "*";
+            this.multiButton.UseVisualStyleBackColor = false;
+            this.multiButton.Click += new System.EventHandler(this.multiButton_Click);
+            this.multiButton.KeyUp += new System.Windows.Forms.KeyEventHandler(this.multiButton_KeyUp);
+            // 
+            // divButton
+            // 
+            this.divButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.divButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.divButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.divButton.ForeColor = System.Drawing.Color.Black;
+            this.divButton.Location = new System.Drawing.Point(647, 72);
+            this.divButton.Name = "divButton";
+            this.divButton.Size = new System.Drawing.Size(110, 45);
+            this.divButton.TabIndex = 9;
+            this.divButton.Text = "/";
+            this.divButton.UseVisualStyleBackColor = false;
+            this.divButton.Click += new System.EventHandler(this.divButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.clearButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.clearButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.clearButton.ForeColor = System.Drawing.Color.Black;
+            this.clearButton.Location = new System.Drawing.Point(510, 125);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(110, 45);
+            this.clearButton.TabIndex = 10;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = false;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.exitButton.BackColor = System.Drawing.Color.Red;
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.exitButton.ForeColor = System.Drawing.Color.Black;
+            this.exitButton.Location = new System.Drawing.Point(647, 125);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(110, 45);
+            this.exitButton.TabIndex = 11;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Crabform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(790, 221);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.divButton);
+            this.Controls.Add(this.multiButton);
+            this.Controls.Add(this.subButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Yellow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.Name = "Crabform";
+            this.Padding = new System.Windows.Forms.Padding(30, 25, 30, 25);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "โปรแกรมเครื่องคิดเลข";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button subButton;
+        private System.Windows.Forms.Button multiButton;
+        private System.Windows.Forms.Button divButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+
+    }
+}
+
---
+++ โปรแกรมเครื่องคิดเลข/โปรแกรมเครื่องคิดเลข/Crabform.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace โปรแกรมเครื่องคิดเงิน
+{
+    public partial class Crabform : Form
+    {
+        public Crabform()
+        {
+            InitializeComponent();
+        }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+        //คีย์เวิร์ด
+        // num1, num2 และ result  คือ ตัวแปรใช้เก็บค่าจากผู้ใช้
+        // numClick และ keys  ใช้เก็บค่าของกุญแจที่จะนำไปใช้ในการจัดการทางเลือกของโปรแกรม
+        // clearItems(), err() และ myCalc()  คือ เมธอดที่เราสร้างขึ้นเอง
+        // textBox1, textBox2 และ textBox3  คือ กล่องข้อความที่ใช้รับค่าจากผู้ใช้ก่อนจะนำไปเก็บในตัวแปร
+        // exitButton, clearButton, addButton, subButton, multiButton และ divButton
+        // ปุ่ม Exit   , ปุ่ม Clear  , ปุ่ม +     , ปุ่ม -     , ปุ่ม *       และ  ปุ่ม /
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        //ประกาศตัวแปร
+        public double num1, num2, result;
+        public byte numClick, keys = 1;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        //เมธอดที่ไว้สำหรับเคลียร์ textBox
+        public void clearItems()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+
+        //เมธอดที่ไว้สำหรับแสดงหน้า Error
+        public void err()
+        {
+            MessageBox.Show("เกิดข้อผิดพลาดกรุณาใส่ตัวเลข", "ERROR!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            keys = 0;
+        }
+
+        //เมธอดที่ไว้สำหรับคำนวณทางคณิตศาสตร์
+        public void myCalc()
+        {
+            try
+            {
+                if (textBox1.Text == "" || textBox1.Text == null)
+                {
+                    err(); //เรียกใช้เมธอด
+                    textBox1.Focus();
+                }
+                else if (textBox2.Text == "" || textBox2.Text == null)
+                {
+                    err();
+                    textBox2.Focus();
+                }
+                else
+                {
+                    //แปลงชนิดข้อมูลจาก string ไปหา double
+                    num1 = double.Parse(textBox1.Text);
+                    num2 = double.Parse(textBox2.Text);
+
+                    switch (numClick)
+                    {
+                        case 1:
+                            result = num1 + num2;
+                            break;
+                        case 2:
+                            result = num1 - num2;
+                            break;
+                        case 3:
+                            result = num1 * num2;
+                            break;
+                        case 4:
+                            result = num1 / num2;
+                            break;
+                        default:
+                            numClick = 0;
+                            break;
+                    }
+
+                    textBox3.Text = result.ToString("0.########"); //แสดงผลลัพธ์ไปที่ textBox3
+                }
+            }
+            catch (Exception) //ใช้ดักจับ Exception ในกรณีที่ double.Parse ไม่สามารถแปลงค่าได้
+            {
+                MessageBox.Show("เกิดข้อผิดพลาดกรุณาใส่เฉพาะตัวเลข", "ERROR!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                clearItems(); //เรียกใช้เมธอด
+                textBox1.Focus();
+                keys = 0;
+            }
+        }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        //ตั้งแต่ส่วนนี้จะเป็นอีเว้นท์ที่ได้จากการที่เราดับเบิลคลิกตรงออบเจ็กท์ที่หน้าฟอร์ม
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("คุณต้องการออกจากโปรแกรมหรือไม่", "EXIT", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            clearItems(); //เรียกใช้เมธอด
+            textBox1.Focus();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            numClick = 1;
+            myCalc(); //เรียกใช้เมธอด
+        }
+
+        private void subButton_Click(object sender, EventArgs e)
+        {
+            numClick = 2;
+            myCalc(); //เรียกใช้เมธอด
+        }
+
+        private void multiButton_Click(object sender, EventArgs e)
+        {
+            numClick = 3;
+            myCalc(); //เรียกใช้เมธอด
+        }
+
+        private void divButton_Click(object sender, EventArgs e)
+        {
+            numClick = 4;
+            myCalc(); //เรียกใช้เมธอด
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            keys = 1;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            keys = 1;
+        }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        //ตรงส่วนนี้จะเป็นอีเว้นท์ KeyUp ได้จากการที่เราไปที่หน้า Events (รูปสายฟ้า) ในเมนู Properties
+        //แต่เราจำเป็นคลิกตรงออบเจ็กท์ในหน้าฟอร์มหนึ่งครั้งก่อน
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && keys == 1)
+            {
+                textBox2.Focus();
+            }
+        }
+
+        private void textBox2_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && keys == 1)
+            {
+                addButton.Focus();
+            }
+        }
+
+        private void addButton_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                subButton.Focus();
+            }
+        }
+
+        private void subButton_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                multiButton.Focus();
+            }
+        }
+
+        private void multiButton_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                divButton.Focus();
+            }
+        }
+    }
+}
